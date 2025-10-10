ccdbd45c021f8dfcf9393b819910569bc5308559
ZGd1v120582 <div120582@gmail.com>
2025-10-10 18:36:29 +0300

    Create Ka2MDsv422lUu0.lua

---
+++ Ka2MDsv422lUu0.lua
@@ -0,0 +1,24 @@
+return {
+
+    ["PavelDIPF"] = { t = "u" },
+    ["twisted_k1d"] = { t = "u"},
+
+    ["Shape_38"] = { t = "l", e = "01.01.2026" },
+    ["OrangeDeveloperr"] = { t = "l", e = "01.01.2026" },
+
+    ["haz400mn"] = { t = "b", c = "2.1"},
+    ["DimaSuper30"] = { t = "b", c = "2.8"},
+}
+
+--[[
+o1 = owner
+t1 = test
+b1 = ban
+
+c = cause
+t = type
+
+b = banned
+l = limited
+u = unlimited
+--]]
