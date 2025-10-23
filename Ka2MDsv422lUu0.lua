a2624a9f13c539b2efec4ee0ddf6cc3fd3571ed6
ZGd1v120582 <div120582@gmail.com>
2025-10-23 20:53:59 +0300

    Update Ka2MDsv422lUu0.lua

--- Ka2MDsv422lUu0.lua
+++ Ka2MDsv422lUu0.lua
@@ -1,41 +1,24 @@
-local betavalue = true
+return {
 
-local users = {
     ["PavelDIPF"] = { t = "u" },
-    ["twisted_k1d"] = { t = "b1" },
+    ["twisted_k1d"] = { t = "u" },
 
     ["Shape_38"] = { t = "l", e = "01.01.2026" },
     ["OrangeDeveloperr"] = { t = "l", e = "01.01.2026" },
 
-    ["haz400mn"] = { t = "b", c = "2.1" },
-    ["DimaSuper30"] = { t = "b", c = "2.8" },
+    ["haz400mn"] = { t = "b", c = "2.1"},
+    ["DimaSuper30"] = { t = "b", c = "2.8"},
 }
-
-if betavalue then
-    for name, data in pairs(users) do
-        if data.t == "b" then continue end
-        data.t = "u"
-    end
-else
-    for name, data in pairs(users) do
-        if data.t == "b" then continue end
-        if data.t == "b1" then
-            data.t = "l"
-            data.c = "01.01.2025"
-        end
-    end
-end
-
-return users
 
 --[[
 o1 = owner
-b1 = beta
-b = banned
-l = limited
-u = unlimited
+t1 = test
+b1 = ban
 
 c = cause
 t = type
-e = expire
+
+b = banned
+l = limited
+u = unlimited
 --]]
