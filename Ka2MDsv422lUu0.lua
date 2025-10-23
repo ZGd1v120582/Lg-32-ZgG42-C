c82fc64e217f5260e12dd338c61177ea88216cb8
ZGd1v120582 <div120582@gmail.com>
2025-10-23 20:49:47 +0300

    Update Ka2MDsv422lUu0.lua

--- Ka2MDsv422lUu0.lua
+++ Ka2MDsv422lUu0.lua
@@ -2,8 +2,6 @@ local betavalue = true
 
 local users = {
     ["PavelDIPF"] = { t = "u" },
---    ["twisted_k1d"] = { t = "u" },
-    
     ["twisted_k1d"] = { t = "b1" },
 
     ["Shape_38"] = { t = "l", e = "01.01.2026" },
@@ -20,9 +18,11 @@ if betavalue then
     end
 else
     for name, data in pairs(users) do
-        if data.t == "b1" then continue end
-        data.t = "l"
-        data.c = "01.01.2025"
+        if data.t == "b" then continue end
+        if data.t == "b1" then
+            data.t = "l"
+            data.c = "01.01.2025"
+        end
     end
 end
 
